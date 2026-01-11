4e7deffc046c37d32eef45d3cba6db8b945464ac
Daniel Allen <dan@aeternagroup.net>
2026-01-11 18:00:22 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "resource_provider_registrations" {
+  description = "All resource_provider_registration resources"
+  value       = azurerm_resource_provider_registration.resource_provider_registrations
+}
+
---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "resource_provider_registrations" {
+  description = <<EOT
+Map of resource_provider_registrations, attributes below
+Required:
+    - name
+Optional:
+    - feature (block):
+        - name (required)
+        - registered (required)
+EOT
+
+  type = map(object({
+    name = string
+    feature = optional(object({
+      name       = string
+      registered = bool
+    }))
+  }))
+}
+
