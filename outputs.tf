0ca61615856de25849fade07024363a057693ab0
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:44 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,12 @@ output "resource_provider_registrations" {
   description = "All resource_provider_registration resources"
   value       = azurerm_resource_provider_registration.resource_provider_registrations
 }
+output "resource_provider_registrations_feature" {
+  description = "List of feature values across all resource_provider_registrations"
+  value       = [for k, v in azurerm_resource_provider_registration.resource_provider_registrations : v.feature]
+}
+output "resource_provider_registrations_name" {
+  description = "List of name values across all resource_provider_registrations"
+  value       = [for k, v in azurerm_resource_provider_registration.resource_provider_registrations : v.name]
+}
 
