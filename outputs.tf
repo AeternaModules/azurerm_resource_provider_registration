d7ea5c5e94ec5ad0c88274d1ddd3e136ba15cd91
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:32 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "resource_provider_registrations_id" {
+  description = "Map of id values across all resource_provider_registrations, keyed the same as var.resource_provider_registrations"
+  value       = { for k, v in azurerm_resource_provider_registration.resource_provider_registrations : k => v.id }
+}
 output "resource_provider_registrations_feature" {
   description = "Map of feature values across all resource_provider_registrations, keyed the same as var.resource_provider_registrations"
   value       = { for k, v in azurerm_resource_provider_registration.resource_provider_registrations : k => v.feature }
